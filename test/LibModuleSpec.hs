dd166531a7c4e439af43a2c063cb2c2710b88beb
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2021-03-17 21:08:16 +0300

    add tgNextOffset test

--- test/LibModuleSpec.hs
+++ test/LibModuleSpec.hs
@@ -6,15 +6,15 @@ import Data.List.Split (splitOn)
 import Data.Maybe (fromJust)
 import Network.URI (URI, parseURI, uriAuthority, uriQuery, uriRegName)
 import Test.Hspec
-import LibModule
+import qualified LibModule as L
 import qualified Types as T
 import qualified Data.ByteString.Char8 as BS
 
 testTelegramConfig :: T.TelegramConfig
 testTelegramConfig =  T.TelegramConfig {T.telegramToken = "TOKEN", T.telegramTimeout = 3}
 
-tgGen :: TgApiUrlGen
-tgGen = mkTgApiUrlGen testTelegramConfig
+tgGen :: L.TgApiUrlGen
+tgGen = L.mkTgApiUrlGen testTelegramConfig
 
 assertTgAuthority :: URI -> Expectation
 assertTgAuthority =
@@ -29,16 +29,21 @@ spec :: Spec
 spec = do
   describe "TgApiUrlGen" $ do
     it "Should generate valid 'getUpdates' URL" $ do
-      let uri = fromJust . parseURI $ getUpdates tgGen 8
+      let uri = fromJust . parseURI $ L.getUpdatesUrl tgGen 8
       assertTgAuthority uri
     it "Should generate valid 'sendMessage' URL" $ do
-      let uri = fromJust . parseURI $ sendMessage tgGen 8 "message!"
+      let uri = fromJust . parseURI $ L.sendMessageUrl tgGen 8 "message!"
       assertTgAuthority uri
       assertQuery (uriQuery uri) ["text=message!", "chat_id=8"]
   describe "Decode VK message.send result" $ do
     it "Should return message id of successful result" $ do
-      let actual = parseVkMessageSendResult (BS.pack "{\"response\":21}")
+      let actual = L.parseVkMessageSendResult (BS.pack "{\"response\":21}")
       actual `shouldBe` Right 21
     it "Should return error of failed result" $ do
-      let actual = parseVkMessageSendResult (BS.pack "{\"error\":{\"error_code\":100,\"error_msg\":\"One of the parameters specified was missing or invalid\"}}")
+      let actual = L.parseVkMessageSendResult (BS.pack "{\"error\":{\"error_code\":100,\"error_msg\":\"One of the parameters specified was missing or invalid\"}}")
       actual `shouldBe` Left "One of the parameters specified was missing or invalid; error code: 100"
+  describe "tgNextOffset" $ do
+    it "Should return original offset if update list is empty" $ do
+      L.tgNextOffset 3 [] `shouldBe` 3
+    it "Should return updates max offset incremented by 1" $ do
+      L.tgNextOffset 3 [T.Update 123 undefined, T.Update 234 undefined] `shouldBe` 235
